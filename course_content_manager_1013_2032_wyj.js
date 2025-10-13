c39111454d40e650ab18baf0f875e4e0fcbd601f
AlBerTo7324 <mrsmileyeagle5@gmail.com>
2025-10-13 14:32:41 +0200

    Enhance 课程内容管理 using ELECTRON

---
+++ course_content_manager_1013_2032_wyj.js
@@ -0,0 +1,97 @@
+// 代码生成时间: 2025-10-13 20:32:39
+// Import required modules
+const { app, BrowserWindow } = require('electron');
+# FIXME: 处理边界情况
+const path = require('path');
+const fs = require('fs');
+const { dialog } = require('@electron/remote');
+
+// Define the CourseContentManager class
+class CourseContentManager {
+  constructor() {
+# 增强安全性
+    this.window = null;
+# 优化算法效率
+  }
+
+  // Initialize the application
+  init() {
+    // Create the main window
+    this.createWindow();
+
+    // Register IPC listeners
+# 扩展功能模块
+    app.on('ready', () => {
+      this.registerIPCListeners();
+    });
+  }
+
+  // Create the main window
+  createWindow() {
+    this.window = new BrowserWindow({
+      width: 800,
+# 扩展功能模块
+      height: 600,
+      webPreferences: {
+# NOTE: 重要实现细节
+        nodeIntegration: true,
+        contextIsolation: false,
+# FIXME: 处理边界情况
+      },
+    });
+
+    // Load the index.html file
+    this.window.loadFile('index.html');
+
+    // Open the DevTools
+# NOTE: 重要实现细节
+    this.window.webContents.openDevTools();
+  }
+
+  // Register IPC listeners
+  registerIPCListeners() {
+    // Add course content
+# 改进用户体验
+    this.window.webContents.on('add-course-content', (event, content) => {
+      this.addCourseContent(content)
+        .then(() => event.reply('add-course-content-success'))
+        .catch((error) => event.reply('add-course-content-error', error.message));
+    });
+# TODO: 优化性能
+  }
+
+  // Add course content to a file
+  addCourseContent(content) {
+    return new Promise((resolve, reject) => {
+# TODO: 优化性能
+      const filePath = path.join(__dirname, 'courses.json');
+      let courses = [];
+
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          return reject(new Error('Unable to read file.'));
+        }
+        courses = JSON.parse(data);
+# NOTE: 重要实现细节
+
+        // Add new content to the courses array
+        courses.push(content);
+
+        // Write the updated content back to the file
+        fs.writeFile(filePath, JSON.stringify(courses, null, 2), (err) => {
+          if (err) {
+# FIXME: 处理边界情况
+            return reject(new Error('Unable to write to file.'));
+          }
+          resolve();
+        });
+      });
+    });
+  }
+}
+
+// Create an instance of the CourseContentManager class
+const manager = new CourseContentManager();
+
+// Start the application
+manager.init();
